3765f5d2626a0db5b50201e179bc108b3a085cd3
pinzonjulian <julianpinzoneslava@gmail.com>
2016-01-30 17:18:45 -0500

    Finish v1. Add close button for all animations and explanation

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,74 @@
-$('.bouncer').click(function (){
-	$(this).addClass('turn-to-ball ani-bounce');
+// ============================================
+// =================Ani Glow===================
+// ============================================
+
+$('.glower').click(function (){
+	$(this).addClass('ani-glow').html('Loading...');
+	$('.glower-exit').show('slow');
 })
 
-function bouncerExit (){
+$('.glower-exit').click(function (){
+	$('.glower').removeClass('ani-glow').html('Click me');
+	$(this).hide('slow');
+})
+
+
+// ============================================
+// =================Ani Glow===================
+// ============================================
+
+$('.rotator').click(function (){
+	$(this).addClass('ani-rotate').html('Loading...');
+	$('.rotator-exit').show('slow');
+})
+
+$('.rotator-exit').click(function (){
+	$('.rotator').removeClass('ani-rotate').html('Click me');
+	$(this).hide('slow');
+})
+
+
+
+// ============================================
+// =================Bouncer====================
+// ============================================
+
+
+$('.bouncer').click(function (){
+	$(this).addClass('turn-to-ball ani-bounce');
+	$('.bouncer-exit').show('slow')
+})
+
+$('.bouncer-exit').click(function (){
 	$('.bouncer').removeClass('turn-to-ball ani-bounce')
-}
+	$(this).hide('slow');
+})
+
+// ============================================
+// =================Clock Step=================
+// ============================================
 
 $('.clock-step').click(function (){
 	$(this).addClass('turn-to-stick ani-clock-step');
+	$('.clock-step-exit').show('slow')
 })
 
-function clockStepExit (){
+$('.clock-step-exit').click(function (){
 	$('.clock-step').removeClass('turn-to-stick ani-clock-step')
-}
+	$(this).hide('slow');
+})
+
+
+// ============================================
+// =================Clock Continous============
+// ============================================
 
 $('.clock-continous').click(function (){
 	$(this).addClass('turn-to-stick ani-clock-continous');
+	$('.clock-continous-exit').show('slow')
 })
 
-function clockContinuosExit (){
+$('.clock-continous-exit').click(function (){
 	$('.clock-continous').removeClass('turn-to-stick ani-clock-continous')
-}
+	$(this).hide('slow');
+})
